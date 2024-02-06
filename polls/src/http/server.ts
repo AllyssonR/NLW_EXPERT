d2f7f375e446d048b3948fd8d8de05c8ffcc61b6
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2024-02-06 20:04:01 -0300

    put end of line ls

--- polls/src/http/server.ts
+++ polls/src/http/server.ts
@@ -1,23 +1,20 @@
-import { fastify } from "fastify";
-import { createPoll } from "../routes/create-poll";
-import { getPoll } from "../routes/get-poll";
-import { voteOnPoll } from "../routes/vote-on-poll";
-import cookie from'@fastify/cookie'
+import { fastify } from 'fastify'
+import { createPoll } from '../routes/create-poll'
+import { getPoll } from '../routes/get-poll'
+import { voteOnPoll } from '../routes/vote-on-poll'
+import cookie from '@fastify/cookie'
 
-
-const app = fastify();
+const app = fastify()
 
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
-app.register(cookie,{
-  secret:'poll-nlw',// for cookies signature
-  hook:'onRequest', //set to false to disable  cookie autoparsing or set autoparsing
-  parseOptions:{} //options for parsing cookies
+app.register(cookie, {
+  secret: 'poll-nlw', // for cookies signature
+  hook: 'onRequest', // set to false to disable  cookie autoparsing or set autoparsing
+  parseOptions: {}, // options for parsing cookies
 })
 
-
-
 app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server listening on");
-});
+  console.log('HTTP server listening on')
+})

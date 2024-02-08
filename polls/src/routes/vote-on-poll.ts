5c89956de77ccb45dd53132354acee8660e291b4
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2024-02-08 17:51:52 -0300

    add redis to count votes

--- polls/src/routes/vote-on-poll.ts
+++ polls/src/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import z from 'zod'
 import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
+import { redis } from '../lib/redis'
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
     const voteOnPollOptons = z.object({
@@ -33,6 +34,7 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id,
           },
         })
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
         return reply
           .status(400)
@@ -56,6 +58,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return reply.status(201).send({ sessionId })
   })
